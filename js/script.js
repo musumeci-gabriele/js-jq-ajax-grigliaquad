aa8f8b258b3d63182be4bb1dd87e799d5d1e8ac8
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-09-16 16:50:49 +0200

    prova

--- js/script.js
+++ js/script.js
@@ -3,6 +3,40 @@ $(document).ready(function(){
   // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
   // Il numero ottenuto appare al centro del quadrato.
 
+  // EVENTO CLICK SUL QUADRATO
+  $(".box").click(function(){
+
+    // CHIAMATA AL SERVER PER LA GENERAZIONE DI UN NUMERO COMPRESO TRA 1 E 9
+    $.ajax(
+      {
+        "url": "https://flynn.boolean.careers/exercises/api/random/int",
+        "method": "GET",
+        "success": function (data, stato) {
+        // RECUPERO DEL NUMERO DAL SERVER
+        var numServer = data.response;
+        numServer = confronto(numServer);
+        document.getElementById("number").innerHTML = numServer;
+      },
+        "error": function (richiesta, stato, errori) {
+        alert("E' avvenuto un errore. " + errore);
+      }
+      }
+    );
+
+    //FUNZIONE DI CONTROLLO SE IL NUMERO ESTRATTO  E' <= 5 O > DI 5
+    // IN UNO DEI DUE CASI IL QUADRATO CAMBIA COLORE E STAMPA IL NUMERO AL SUO INTERNO
+    function confronto(numServer){
+      if (numServer <= 5) {
+        $(".box").addClass(".yellow");
+      }else {
+        $(".box").addClass(".green");
+      }
+    }
+
+  });
+
+
+
 
 
 
