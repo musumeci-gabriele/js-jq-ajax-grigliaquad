4992f59a74e0d7837b11d3da3c1c565dcd0ba9f1
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-09-16 15:09:10 +0200

    Start

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  // Generare una griglia 6x6 (36 boxes), ad ogni click parte una richiesta AJAX che prende un numero random da 1 a 9 (scegliere API opportuna).
+  // Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
+  // Il numero ottenuto appare al centro del quadrato.
+
+
+
+
+
+});
